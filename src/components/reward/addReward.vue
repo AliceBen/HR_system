76f6e27b468eeb24e7bb8369329cc0812129975a
AliceBen <angelQin0704@163.com>
2020-04-12 15:22:41 +0800

    fix:奖惩绑定员工id

--- src/components/reward/addReward.vue
+++ src/components/reward/addReward.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="wrap">
     <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="所属员工">
+        <el-select @change="optionsChange" v-model="value" placeholder="请选择">
+          <el-option v-for="item in options" :key="item.id" :label="item.name" :value="item.id"></el-option>
+        </el-select>
+      </el-form-item>
+       <el-form-item label="员工编号">
+        <el-input v-model="form.userId" disabled></el-input>
+      </el-form-item>
       <el-form-item label="奖惩名称">
         <el-input v-model="form.name"></el-input>
       </el-form-item>
@@ -31,7 +39,10 @@ import axios from 'axios'
 export default {
   data() {
     return {
+      options:'',
+      value:'',
       form: {
+        userId:'',
         id:Math.ceil(Math.random()*100),
         name: '',
         reason: '',
@@ -40,7 +51,25 @@ export default {
       }
     }
   },
+  created() {
+  this.getUserList()
+  },
   methods: {
+    optionsChange(val){
+      this.form.userId = val
+      console.log(val,'=====')
+    },
+     // 获取列表数据
+    getUserList() {
+      axios.get('/api/staffList/pages').then(res => {
+        if (res.data.code !== 'OK') {
+          return this.$message.error('信息查询失败')
+        } else {
+          this.options = res.data.data
+          console.log(this.users, '=====')
+        }
+      })
+    },
     onSubmit() {
       axios.post('/api/prizePunishList/add', this.form).then(res => {
         if (res.data.code !== 'OK') {
@@ -67,5 +96,8 @@ export default {
 }
 .el-date-editor.el-input {
   width: 100%;
+}
+.el-select {
+   width: 100%;
 }
 </style>
